7583bee3b0af9b553674dacf1f8664ab935340f2
rayyanp <102375875+rayyanp@users.noreply.github.com>
2022-04-29 17:39:42 +0100

    Add files via upload
    Overhauled the diary page so that the user can input their food, calories and view their diary/current days food, meal type, calories and time.

--- diary.php
+++ diary.php
@@ -10,27 +10,30 @@ else
 {
   $uid=$_SESSION['uid'];
 }
+//insert loop for days
 
-if(isset($_POST['ASC']))
+//Current day
+if(isset($_POST['plus']))
 {
-    $asc_query = "SELECT * FROM calories WHERE uid = $uid ORDER BY cid ASC AND date(datetime) = curdate()";
-    $result = executeQuery($asc_query);
+  $plus_query = "SELECT meal,food,calorie_intake, CAST(datetime as time) AS 'Time' FROM calories WHERE uid = $uid AND date(datetime) = curdate();";
+  $result = executeQuery($plus_query);
 }
 
-// Descending Order
-elseif (isset ($_POST['DESC'])) 
-    {
-          $desc_query = "SELECT * FROM calories WHERE uid = $uid ORDER BY cid DESC AND date(datetime) = curdate()";
-          $result = executeQuery($desc_query);
-    }
-    
-    // Default Order
- else {
-        $default_query = "SELECT * FROM calories WHERE uid = $uid AND date(datetime) = curdate()";
-        $result = executeQuery($default_query);
+// Previous day
+elseif (isset ($_POST['minus'])) 
+{
+  $minus_query = "SELECT meal,food,calorie_intake, CAST(datetime as time) AS 'Time' FROM calories WHERE uid = $uid AND date(datetime) = curdate()-1;";
+  $result = executeQuery($minus_query);
+}
+
+//Current day
+else 
+{
+  $default_query = "SELECT meal,food,calorie_intake, CAST(datetime as time) AS 'Time' FROM calories WHERE uid = $uid AND date(datetime) = curdate();";
+  $result = executeQuery($default_query);
 }
 ?>
-      <h3>Today</h3><br>
+      <h3><center>Diary<center></h3><br>
         <div class="col-md-8 m-auto block" id="main_content">
       <h3>Add your calories</h3><br>
         <form action="calorieupdate.php" method="post" enctype="multipart/form-data">
@@ -54,46 +57,21 @@ elseif (isset ($_POST['DESC']))
           <div class="col-sm-2 col-sm-offset-5">
             <input type="text" class="form-control" id="inputCal" placeholder="Total" name="calorie_intake">
             </div>
-          </div>
-
-          <div class="form-group">
-          <div class="col-sm-12">
-              <label for="inputFood" class="col-sm-12">What exercise did you do?</label>
-              </div>
-          </div>
-              <div class="form-group">
-              <div class="col-sm-4 col-sm-offset-4">
-                  <input type="text" class="form-control" id="inputFood" placeholder="Title" name="exercise">
-              </div>
-          </div>
-          
-          <div class="form-group">
-          <div class="col-sm-12">
-              <label for="inputCal" class="col-sm-12">How many calories were burnt?</label>
-              </div>
-          </div>
-          <div class="form-group">
-          <div class="col-sm-2 col-sm-offset-5">
-            <input type="text" class="form-control" id="inputCal" placeholder="Total" name="calories_burnt">
-            </div>
           </div><br>
-            <button class="btn btn-success" type="submit" name="post">Submit</button>
+            <button class="btn btn-outline-primary" type="submit" name="post">Add</button>
         </form>
       </div> 
     </div><br>
-    
     <div class="col-md-8 m-auto block" id="main_content">
-    <h3>Todays calories</h3><br>
-      <form action="history.php" method="post">
-        <input type="submit" class = "btn btn-info" name="ASC" value="Ascending">
-        <input type="submit" class = "btn btn-info" name="DESC" value="Descending"><br><br>
+      <form action="diary.php" method="post">
+      <input type="submit" class = "btn btn-outline-primary" name="minus" value="Previous Day">
+      <input type="submit" class = "btn btn-outline-primary" name="plus" value="Current Day">
     <table class="table-main">
     <tr>
+      <th>Meal</th>
       <th>Food</th>
-      <th>Calorie Intake</th>
-      <th>Exercise</th>
-      <th>Calories Burnt</th>
-      <th>Date</th>
+      <th>Calories</th>
+      <th>Time</th>
     </tr>
 <?php
 function executeQuery($query)
@@ -106,11 +84,10 @@ function executeQuery($query)
 while($row = mysqli_fetch_assoc($result)){
   ?> 
 <tr>
+    <td><?php echo $row['meal'];?></td>
     <td><?php echo $row['food'];?></td>
     <td><?php echo $row['calorie_intake'];?></td>
-    <td><?php echo $row['exercise'];?></td>
-    <td><?php echo $row['calories_burnt'];?></td>
-    <td>Date:<?php echo $row['datetime'];?></td>
+    <td><?php echo $row['Time'];?></td>
 </tr>
   <?php
 }
